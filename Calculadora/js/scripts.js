d7f25424b5406ba122f0bb185ea142b36bd5cf5d
Gleisson Santos <jsgleisson@gmail.com>
2022-11-30 22:53:00 -0300

    commit

--- Calculadora/js/scripts.js
+++ Calculadora/js/scripts.js
@@ -42,11 +42,18 @@ const data = [
 
   const imcTable =  document.querySelector("#imc-table");
 
-  const heighInput = document.querySelector("#height");
+  const heightInput = document.querySelector("#height");
   const weightInput = document.querySelector("#weight");
   const calcBtn = document.querySelector("#calc-btn");
   const clearBtn = document.querySelector("#clear-btn");
 
+  const calcContainer = document.querySelector("#calc-container")
+  const resultContainer = document.querySelector("#result-container")
+
+  const imcNumber = document.querySelector("#imc-number span")
+  const imcinfo = document.querySelector("#imc-info span")
+
+  const backBtn = document.querySelector("#back-btn")
 
   //Funções
 
@@ -74,7 +81,109 @@ const data = [
     });
   }
 
+  //Limpar inputs
+  function cleanInputs() {
+    heightInput.value = "";
+    weightInput.value = "";
+    imcNumber.classList = "";        
+    imcinfo.classList = "";
+
+  }
+
+  //Replace
+  function validDigits(text) {
+    return text.replace(/[^0-9,]/g, "")
+  }
+
+  //Calculo do IMC
+  function calcImc(weight, height) {
+    const imc = (weight / (height * height)).toFixed(1);
+    return imc;
+  }
+  
+
+  function showOrdeResults() {
+    calcContainer.classList.toggle("hide")
+    resultContainer.classList.toggle("hide")
+  }
   //Inicialização do projeto
   createTable(data);
 
   //Eventos
+  [heightInput, weightInput].forEach((el) => {
+      el.addEventListener("input", (e) => {
+        const updatedValue = validDigits(e.target.value);
+
+         e.target.value = updatedValue;
+
+       }); 
+  });
+
+  //Conversão de numeros para interios e replace de , e .
+  calcBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+  
+    const weight = +weightInput.value.replace(",", ".");
+    const height = +heightInput.value.replace(",", ".");
+  
+    console.log(weight, height);
+  
+    if (!weight || !height) return;
+  
+    //A ordem importa!!!!!!!
+    const imc = calcImc(weight, height);
+   
+    let info;
+
+    data.forEach((item) => {
+      if (imc >= item.min && imc <= item.max) {
+        info = item.info;
+      }
+    });
+
+    console.log(info);
+
+    if (!info) return;
+
+    imcNumber.innerText = imc
+    imcinfo.innerText = info
+
+    switch(info) {
+      case "Magreza":
+        imcNumber.classList.add("low");        
+        imcinfo.classList.add("low");
+        break;        
+      case "Normal":
+        imcNumber.classList.add("good");        
+        imcinfo.classList.add("good");
+        break;        
+      case "Sobrepeso":
+        imcNumber.classList.add("low");        
+        imcinfo.classList.add("low");
+        break;        
+      case "Obesidade":
+        imcNumber.classList.add("medium");        
+        imcinfo.classList.add("medium");
+        break;        
+      case "Obesidade grave":
+        imcNumber.classList.add("high");        
+        imcinfo.classList.add("high");
+        break;        
+    }
+
+    showOrdeResults();
+
+  });
+
+  clearBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+  
+    cleanInputs();
+  });
+
+
+
+  backBtn.addEventListener("click", () => {
+    cleanInputs();
+    showOrdeResults();
+  })
